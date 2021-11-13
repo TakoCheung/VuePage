4ec75a1a70f5e327ab59a3688254f36ec4920204
Tako Cheung <takocheung@MacBook-Pro.local>
2021-11-13 09:10:30 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div id="app">
+ <!--<v-header :title=this.$route.name></v-header>-->
+  <nav class="main-nav">
+   <Burger/>
+   <router-view/>
+
+   <Sidebar>
+    <ul class="sidebar-panel-nav">
+     <li @click="toggle"><router-link to="/">Welcome</router-link></li>
+     <li @click="toggle"><router-link to="/about">About</router-link></li>
+     <li @click="toggle"><router-link to="/education">Education</router-link></li>
+     <li @click="toggle"><router-link to="/WorkExpriences">Work Expriences</router-link></li>
+     </ul>
+    </Sidebar>
+  </nav>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Burger from './components/Menu/Burger.vue';
+import Sidebar from './components/Menu/Sidebar.vue';
+import { store, mutations } from './store.js'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+ name: 'app',
+ components: {
+   Sidebar,
+   Burger
+ },
+ computed: {
+     isBurgerActive() {
+         return store.isNavOpen
+     }
+ },
+ methods: {
+     toggle() {
+         mutations.toggleNav()
+     }
+ }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+html {
+   height: 100%;
+   overflow:auto;
+ }
+
+ body {
+   border: 0; margin: 0; padding: 0;
+   font-family: 'Lato';
+   height: 100%;
+   background: rgba(0, 128, 255, 0.2)/*linear-gradient(45deg, rgba(101,31,87,1) 0%, rgba(225,113,87,5) 48%, rgba(249,248,113,1) 100%);*/
+ }
+
+ .logo {
+   align-self: center;
+   color: #fff;
+   font-weight: bold;
+   font-family: 'Lato'
+ }
+
+ .main-nav {
+   display: flex;
+   justify-content: space-between;
+   padding: 0.5rem 0.8rem;
+ }
+
+ ul.sidebar-panel-nav {
+   list-style-type: none;
+ }
+
+ ul.sidebar-panel-nav > li > a {
+   color: #fff;
+   text-decoration: none;
+   font-size: 1.5rem;
+   display: block;
+   padding-bottom: 0.5em;
+ }
+
+ li:hover,
+ li.router-link-active,
+ li.router-link-exact-active {
+   background-color: #000000;
+   text-decoration: underline;
+ }
+
+ /* play with some lines below */
+ #progress-container-el {
+   /* background */
+   background-color: transparent !important;
+ }
+ #progress-el {
+   /* progress bar */
+   background-color: #4285f4 !important;
+ }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import VueScrollProgress from 'vue-scroll-progress'
+import Header from 'v-header'
+import VueGoogleCharts from 'vue-google-charts'
+import vuetify from './plugins/vuetify'
+
+Vue.use(VueGoogleCharts)
+Vue.use(VueScrollProgress)
+Vue.use(Header)
 
 Vue.config.productionTip = false
-
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
