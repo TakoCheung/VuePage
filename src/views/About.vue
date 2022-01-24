f23bb44527a7905acfedfcd6bdb79264185c74c7
Tako <tako520cn@gmail.com>
2022-01-23 21:49:23 -0500

    go to sleep

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,34 +2,34 @@
 
     <div class="about">
       <h1>About</h1>
-      <p>Looking for a chance to impact the community or even the world that <br>I live in with my passion.</p>
+      <p>Looking for a chance to impact the community or even the world that I live in with my passion.</p>
 
       <h3>Algorithm</h3>
-      <p>Life is just like a game, so I love playing a game. How the game makes you <br>
-         use your brain is a determination of how good the game is. The harder the game<br>
-         makes you think, the better the game is. No matter how good the game is, It will<br>
-         come to an end, but algorithm won't. One of the reasons of why I am passionate <br>
+      <p>Life is just like a game, so I love playing a game. How the game makes you
+         use your brain is a determination of how good the game is. The harder the game
+         makes you think, the better the game is. No matter how good the game is, It will
+         come to an end, but algorithm won't. One of the reasons of why I am passionate
          about coding is the endless algorithm.</p>
 
       <h3>Hardware and Software</h3>
-      <p>While I worked in the cell phone repair shop, I fixed cell phone step by step<br>
-         most of the time. When I encounter some problem like Wi-Fi is not working, or<br>
-         Bluetooth is not working. I diagnosed what the problem is, and I found that I<br>
-         could solve the problem either by software or hardware. One of the reasons of<br>
-         why I am passionate about coding is the hardware and software are closely related,<br>
-         and I can work on both at the same time. I am persistent because I am doing what<br>
+      <p>While I worked in the cell phone repair shop, I fixed cell phone step by step
+         most of the time. When I encounter some problem like Wi-Fi is not working, or
+         Bluetooth is not working. I diagnosed what the problem is, and I found that I
+         could solve the problem either by software or hardware. One of the reasons of
+         why I am passionate about coding is the hardware and software are closely related,
+         and I can work on both at the same time. I am persistent because I am doing what
          I am passionate about.</p>
 
       <h3>Skillful</h3>
-      <p>My skills are mostly obtained from the school, but they are sharpened by practice.<br>
-         All the programming language could be sharpened by practice, yet the communication<br>
-         skill could be sharpened by listening to the others.The main ingredient of a successful<br>
-         communication is listening, and one of the bases of leadership is communication.</p>
+      <p>My skills are mostly obtained from the school, but they are sharpened by practice.
+         All the programming language could be sharpened by practice, yet the communication
+         skill could be sharpened by listening to the others.The main ingredient of a successful
+         communication is listening, and one of the bases of leadership is communication.
 
       <h3>Stable and Volatile</h3>
-      <p>I am stable and organized when the team needs any execution. Any executions must<br>
-      have a plan behind. Any plans must have organized details supported. I am volatile<br>
-      when the team needs any innovation. Innovation needs flexibility. Flexibility needs<br>
+      <p>I am stable and organized when the team needs any execution. Any executions must
+      have a plan behind. Any plans must have organized details supported. I am volatile
+      when the team needs any innovation. Innovation needs flexibility. Flexibility needs
       open-mindedness. I am a mix of two like the majority of people.</p>
 
       <p>If you are looking for a passionate employee, you could email me at
